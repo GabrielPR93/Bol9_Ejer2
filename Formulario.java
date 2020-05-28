8fbf70874620dbe0793490d71fa023cc236d449b
GAbriel Pérez Rodríguez <gabrielpr93@hotmail.com>
2020-05-28 17:33:26 +0200

    Ejercicio2

---
+++ Formulario.java
@@ -0,0 +1,202 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.util.Scanner;
+
+public class Formulario extends JFrame implements ActionListener {
+    JButton b;
+    JButton btnReset;
+    JTextField txf;
+    String acu = "";
+    JMenuBar mnuPrincipal;
+    JMenu mnuArchivo, mnuMovil, mnuOtros;
+    JMenuItem mnuGrabar, mnuLeer, mnuReset, mnuSalir, mnuInformacion;
+
+    private String[] teclado = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "#", "0", "*" };
+    Color color = Color.green;
+    Color colorBase = null;
+
+    String home = System.getProperty("user.home");
+    File archivo = new File("/Telefono.txt");
+
+    public Formulario() {
+        super("Teléfono móvil");
+        setLayout(null);
+
+        int x = 10, y = 100;
+        for (int i = 0; i < teclado.length; i++) {
+
+            b = new JButton(teclado[i]);
+            b.setSize(100, 20);
+            b.setLocation(x, y);
+            b.setBackground(colorBase);
+            b.addMouseMotionListener(new Raton());
+            b.addMouseListener(new Raton());
+            b.addActionListener(this);
+            this.add(b);
+
+            if ((i + 1) % 3 == 0) {
+                x = 10;
+                y += 40;
+            } else {
+                x += 210;
+            }
+        }
+
+        txf = new JTextField();
+        txf.setSize(520, 50);
+        txf.setLocation(10, 10);
+        txf.addActionListener(this);
+        txf.addKeyListener(new Teclado());
+        txf.setEditable(false);
+        add(txf);
+
+        btnReset = new JButton("Reset");
+        btnReset.setSize(100, 20);
+        btnReset.setLocation(220, 270);
+        btnReset.addActionListener(this);
+        btnReset.addKeyListener(new Teclado());
+        add(btnReset);
+
+        // Menú Archivo
+        mnuGrabar = new JMenuItem("Grabar");
+        mnuGrabar.setMnemonic('G');
+        mnuGrabar.addActionListener(this);
+
+        mnuLeer = new JMenuItem("Leer");
+        mnuLeer.setMnemonic('L');
+        mnuLeer.addActionListener(this);
+
+        mnuArchivo = new JMenu("Archivo");
+        mnuArchivo.add(mnuGrabar);
+        mnuArchivo.setMnemonic('G');
+        mnuArchivo.add(mnuLeer);
+
+        // Menú Móvil
+        mnuReset = new JMenuItem("Reset");
+        mnuReset.setMnemonic('R');
+        mnuReset.addActionListener(this);
+
+        mnuSalir = new JMenuItem("Salir");
+        mnuSalir.setMnemonic('S');
+        mnuSalir.addActionListener(this);
+
+        mnuMovil = new JMenu("Móvil");
+        mnuMovil.add(mnuReset);
+        mnuMovil.addSeparator();
+        mnuMovil.add(mnuSalir);
+
+        // Menú Otros
+        mnuInformacion = new JMenuItem("Información");
+        mnuInformacion.setMnemonic('I');
+        mnuInformacion.addActionListener(this);
+
+        mnuOtros = new JMenu("Otros");
+        mnuOtros.add(mnuInformacion);
+
+        // Menú Principal
+
+        mnuPrincipal = new JMenuBar();
+        mnuPrincipal.add(mnuArchivo);
+        mnuPrincipal.add(mnuMovil);
+        mnuPrincipal.add(mnuOtros);
+        this.setJMenuBar(mnuPrincipal);
+
+    }
+
+    private class Raton extends MouseAdapter {
+
+        @Override
+        public void mouseMoved(MouseEvent e) {
+            ((JButton) e.getSource()).setBackground(color);
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+            ((JButton) e.getSource()).setBackground(colorBase);
+            colorBase = null;
+        }
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            if (e.getClickCount() >= 1) {
+                colorBase = Color.red;
+                ((JButton) e.getSource()).setBackground(colorBase);
+            }
+        }
+
+    }
+
+    private class Teclado extends KeyAdapter {
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            if (e.getKeyCode() >= KeyEvent.VK_0 && e.getKeyCode() <= KeyEvent.VK_9) {
+                acu += e.getKeyChar();
+
+            }
+        }
+
+        @Override
+        public void keyTyped(KeyEvent e) {
+            txf.setText(acu);
+
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource().getClass() == JButton.class) {
+            acu += e.getActionCommand();
+            txf.setText(acu);
+
+        }
+
+        if (e.getSource() == mnuGrabar) {
+            try (PrintWriter f = new PrintWriter(new FileWriter(home + archivo, true))) {
+                f.println(acu);
+            } catch (Exception ex) {
+                System.err.println("Error de acceso al archivo");
+            }
+        }
+
+        if (e.getSource() == mnuLeer) {
+
+            String contenido;
+            try (Scanner f = new Scanner(new File(home + archivo))) {
+                while (f.hasNext()) {
+                    contenido = f.nextLine();
+                    JOptionPane.showMessageDialog(null, contenido, "Numeros", JOptionPane.INFORMATION_MESSAGE);
+                }
+            } catch (Exception s) {
+                System.err.println("Error de Accesso al Archivo");
+            }
+        }
+
+        if (e.getSource() == btnReset || e.getSource() == mnuReset) {
+            acu = "";
+            txf.setText(null);
+            for (int i = 0; i < teclado.length; i++) {
+                b.setBackground(null);
+
+            }
+        }
+        if (e.getSource() == mnuSalir) {
+
+            int respuesta;
+            respuesta = JOptionPane.showConfirmDialog(null, "Estas seguro que quieres salir de la aplicación ?",
+                    "Salir", JOptionPane.OK_CANCEL_OPTION);
+            if (respuesta == JOptionPane.OK_OPTION) {
+                System.exit(0);
+            }
+        }
+
+        if (e.getSource() == mnuInformacion) {
+
+            JOptionPane.showMessageDialog(null, "Directorio actual: " + System.getProperty("user.dir") + "\n"
+                    + "Usuario: " + System.getProperty("user.home"), "Información", JOptionPane.INFORMATION_MESSAGE);
+        }
+    }
+
+}
---
+++ Principal.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class Principal {
+    public static void main(String[] args) throws Exception {
+        Formulario f = new Formulario();
+
+        f.setSize(600, 400);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setVisible(true);
+
+    }
+}
